f91f1342731de6b981bded6a7568ba73b0973206
Tiki92 <burca_vlad92@yahoo.com>
2018-09-01 15:31:42 +0100

    Added Search functionality

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from add_code.models import Code
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -9,6 +10,11 @@ def index(request):
 
     page = request.GET.get('page')
     list_code = paginator.get_page(page)
+    query = request.GET.get('q', None)
+    if query is not None:
+        list_code = page_code.filter(
+            Q(Title__icontains=query) |
+            Q(Content__icontains=query))
     return render(request, 'webapp/home.html', {'list_code': list_code})
 
 
@@ -16,7 +22,6 @@ def detailed(request, code_id):
     detail = Code.objects.all()
     post = Code.objects.get(id=code_id)
     return render(request, 'webapp/detailed_view.html', {'detail': detail, 'post': post})
-
 
 
 
